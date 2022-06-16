f9e303fc8242e7ded2b24e6e6edccb4767824456
TimurDavlet <timkoootim@yandex.ru>
2022-06-16 13:28:24 +0400

    fix tests

--- __tests__/useInputState.test.js
+++ __tests__/useInputState.test.js
@@ -16,34 +16,34 @@ describe("useInputState tests", () => {
     it("addTodo", () => {
       const { result } = renderHook(
         (initialProps) => useToDoState(initialProps),
-        {initialProps: [{active: false, task: 'test'}]}
+        {initialProps: [{active: false, task: 'test', id: 1 }]}
       );
       const { todos, addTodo } = result.current;
       expect(addTodo).toBeDefined();
-      expect(todos).toEqual([{active: false, task: 'test'}]);
+      expect(todos).toEqual([{active: false, task: 'test', id: 1 }]);
     });
 
     it("deleteTodo", () => {
-      const { result } = renderHook(() => useToDoState([{active: false, task: 'test1'}, {active: false, task: 'test2'}]));
+      const { result } = renderHook(() => useToDoState([{active: false, task: 'test1', id: 1}, {active: false, task: 'test2', id: 2}]));
       const { todos, deleteTodo } = result.current;
-      expect(todos).toEqual([{active: false, task: 'test1'}, {active: false, task: 'test2'}]);
-      deleteTodo(1);
-      expect(result.current.todos).toEqual([{active: false, task: 'test1'}]);
+      expect(todos).toEqual([{active: false, task: 'test1', id: 1}, {active: false, task: 'test2', id: 2}]);
+      deleteTodo(2);
+      expect(result.current.todos).toEqual([{active: false, task: 'test1', id: 1}]);
     });
 
     it("deleteCompletedToDo", () => {
-      const { result } = renderHook(() => useToDoState([{active: true, task: 'test1'}, {active: false, task: 'test2'}]));
+      const { result } = renderHook(() => useToDoState([{active: true, task: 'test1', id: 1}, {active: false, task: 'test2', id: 2}]));
       const { todos, deleteCompletedToDo } = result.current;
-      expect(todos).toEqual([{active: true, task: 'test1'}, {active: false, task: 'test2'}]);
+      expect(todos).toEqual([{active: true, task: 'test1', id: 1}, {active: false, task: 'test2', id: 2}]);
       deleteCompletedToDo();
-      expect(result.current.todos).toEqual([{active: false, task: 'test2'}]);
+      expect(result.current.todos).toEqual([{active: false, task: 'test2', id: 2}]);
     });
 
     it("activTodoTask", () => {
-      const { result } = renderHook(() => useToDoState([{active: true, task: 'test1'}, {active: false, task: 'test2'}]));
+      const { result } = renderHook(() => useToDoState([{active: true, task: 'test1', id: 1}, {active: false, task: 'test2', id: 2}]));
       const { todos, activTodoTask } = result.current;
-      expect(todos).toEqual([{active: true, task: 'test1'}, {active: false, task: 'test2'}]);
-      activTodoTask(1);
-      expect(result.current.todos).toEqual([{active: true, task: 'test1'}, {active: true, task: 'test2'}]);
+      expect(todos).toEqual([{active: true, task: 'test1', id: 1}, {active: false, task: 'test2', id: 2}]);
+      activTodoTask(2);
+      expect(result.current.todos).toEqual([{active: true, task: 'test1', id: 1}, {active: true, task: 'test2', id: 2}]);
     });
   });
